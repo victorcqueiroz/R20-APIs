eebfef826ba039e9a567af5d0a1edfdadadf15b6
victorcqueiroz <31393982+victorcqueiroz@users.noreply.github.com>
2019-04-01 14:50:29 -0300

    Update wildmagic.js

--- wildmagic.js
+++ wildmagic.js
@@ -4,17 +4,10 @@
 
 var Wild_Magic = Wild_Magic || (function() {
     'use strict';
-
-    var version = "0.0.1";
-});
-
-function countDown() {
-    'use strict';
-
-    //wildmagic = wildmagic + 1;
 	var wildmagic = 2;
     if (wildmagic == 1 || wildmagic == 2) {
-        sendChat("Magia Selvagem!" (wildmagic): Role nessa tabela, no começo de cada um dos seus turnos pelo proximo minuto, ignorando esse resultado em rolagens subsequentes.");
+        	sendChat("Magia Selvagem!" (wildmagic): Role nessa tabela, no começo de cada um dos seus turnos pelo proximo minuto, ignorando esse resultado em rolagens subsequentes.");
 	}
+});
 
 
