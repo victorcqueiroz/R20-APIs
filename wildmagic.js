0794cb08208e19a1e6a469b6713b6e40a567c313
victorcqueiroz <31393982+victorcqueiroz@users.noreply.github.com>
2019-04-01 14:25:47 -0300

    Update wildmagic.js

--- wildmagic.js
+++ wildmagic.js
@@ -1,6 +1,6 @@
 // Created By: Victor Queiroz
 
-var wildmagic = Math.round(Math.random()*100);
+//var wildmagic = Math.round(Math.random()*100);
 
 var Wild_Magic = Wild_Magic || (function() {
     'use strict';
@@ -11,13 +11,13 @@ var Wild_Magic = Wild_Magic || (function() {
 function countDown() {
     'use strict';
 
-    wildmagic = wildmagic + 1;
-
+    //wildmagic = wildmagic + 1;
+	var wildmagic = 2;
     if (wildmagic == 1 || wildmagic == 2) {
-        sendChat("Magia Selvagem!" (wildmagic): Role nessa tabela, no começo de cada um dos seus turnos pelo proximo minuto, ignorando esse resultado em rolagens subsequentes.");
+        sendChat("Magia Selvagem!" (wildmagic): Role nessa tabela, no comeÃ§o de cada um dos seus turnos pelo proximo minuto, ignorando esse resultado em rolagens subsequentes.");
     }
 	if (wildmagic == 3 || wildmagic == 4) {
-        sendChat("Magia Selvagem!" (wildmagic): Pelo proximo minuto, voce pode ver qualquer criatura invisivel, se você tiver linha de visao");
+        sendChat("Magia Selvagem!" (wildmagic): Pelo proximo minuto, voce pode ver qualquer criatura invisivel, se vocÃª tiver linha de visao");
     }
 }
 
